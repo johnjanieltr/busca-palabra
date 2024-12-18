efef38e7dd500b2aa466d55ea93d86e9253d2235
johnjanieltr <troyajohn17@gmail.com>
2024-12-18 18:36:50 -0400

    Adding functionality and change lang Language changed in the html file Added a functionality to print row Added functionality when user press some key

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 import words from "./words.js";
 
-document.addEventListener("keyup", (e) => {
-  console.log(e);
+let wordLengthMode = 5;
+let currentRow = 0;
+let currentItem = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  printRow();
 });
 
+document.addEventListener("keyup", (e) => {
+  // delete last
+  const row = document.getElementById(`row-${currentRow}`);
+
+  if (e.key === "Backspace"){
+    console.log("back");
+    return;
+  }
+  // full row
+  if (currentItem === wordLengthMode) {
+    row.children[currentItem].textContent = e.key;
+    console.log("done");
+    return;
+  }
+
+  row.children[currentItem].textContent = e.key;
+  currentItem++;
+});
+
+// FUNCTIONS
 const randomWord = () => {
   let randomNumber = Math.round(Math.random() * words.length);
+  if (!randomNumber) console.log("Ah ocurrido un error");
   return words[randomNumber];
 };
 
-console.log(randomWord());
+const printRow = () => {
+  currentRow++;
+  const $board = document.getElementById("board");
+  const container = document.createElement("div");
+  container.classList.add("row", "gap-2");
+  container.id = "row-1";
+
+  for (let i = 0; i < wordLengthMode; i++) {
+    container.insertAdjacentHTML("beforeend",
+      "<div class='d-flex justify-content-center align-items-center item'></div>"
+    );
+  };
+  
+  $board.insertAdjacentElement("beforeend", container);
+};
